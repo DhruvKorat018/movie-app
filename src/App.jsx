fab5f99ae4b302e80f826f4baa5e1b7edfc2895c
DhruvKorat018 <kdkorat.kd@gmail.com>
2024-02-18 17:10:08 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold h-[100vh] w-[100vw] flex items-center justify-center'>This practice movie app is in devlopement</div>
+  )
+}
+
+export default App
